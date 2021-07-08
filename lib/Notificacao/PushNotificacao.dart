6f329ef47a77b19ad78c83dd2a0d615651f11487
Marlos <marlostrinidad@gmail.com>
2021-07-08 18:59:13 -0300

    atualizado

--- lib/Notificacao/PushNotificacao.dart
+++ lib/Notificacao/PushNotificacao.dart
@@ -6,35 +6,79 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:motoboybug_app/Model/Pedido.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'NotificacaoDialog.dart';
 class PushNotificacao{
-  final FirebaseMessaging firebaseMessaging=FirebaseMessaging();
-  Future initialize(context) async {
+  final FirebaseMessaging firebaseMessaging=FirebaseMessaging.instance;
+  Pedido detalheCorrida=Pedido();
 
 
 
-      firebaseMessaging.configure(
-        onMessage: (Map<String, dynamic> message) async {
-          print("onMessage: $message");
-          MostrarInformacaodaNotificacao(context,message);
+  Future<void> initialize(context) async {
+    await Firebase.initializeApp();
+    const AndroidNotificationChannel channel = AndroidNotificationChannel(
+      'high_importance_channel', // id
+      'High Importance Notifications', // title
+      'This channel is used for important notifications.', // description
+      importance: Importance.high,
+    );
+    // Set the background messaging handler early on, as a named top-level function
+    FirebaseMessaging.onBackgroundMessage(initialize);
 
-        },
-        onLaunch: (Map<String, dynamic> message) async {
-          print("onLaunch: $message");
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+    /// Create an Android Notification Channel.
+    ///
+    /// We use this channel in the `AndroidManifest.xml` file to override the
+    /// default FCM channel to enable heads up notifications.
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+        AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
 
+    /// Update the iOS foreground notification presentation options to allow
+    /// heads up notifications.
+    await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
 
-        },
-        onResume: (Map<String, dynamic> message) async {
-          print("onResume: $message");
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification notification = message.notification;
+      AndroidNotification android = message.notification?.android;
+      if (notification != null && android != null) {
+        detalheCorrida.lat_ponto="latponto.toString()";
+        detalheCorrida.long_ponto="longponto.toString()";
+        detalheCorrida.end_ponto="end_ponto";
+        detalheCorrida.nome_ponto="nome_ponto";
+        detalheCorrida.quant_itens="4";
+        detalheCorrida.telefone="telefone";
+        detalheCorrida.id_doc="id_Doc";
+        showDialog(
+            context:context,
+            barrierDismissible: false,
+            builder: (BuildContext context)=>NotificacaoDialog(detalheCorrida:detalheCorrida) );
+      }
+    });
 
-          MostrarInformacaodaNotificacao(context,message);
-
-
-
-        },
-      );
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      detalheCorrida.lat_ponto="latponto.toString()";
+      detalheCorrida.long_ponto="longponto.toString()";
+      detalheCorrida.end_ponto="end_ponto";
+      detalheCorrida.nome_ponto="nome_ponto";
+      detalheCorrida.quant_itens="4";
+      detalheCorrida.telefone="telefone";
+      detalheCorrida.id_doc="id_Doc";
+      showDialog(
+          context:context,
+          barrierDismissible: false,
+          builder: (BuildContext context)=>NotificacaoDialog(detalheCorrida:detalheCorrida) );
+    }
+    );
   }
 
   void MostrarInformacaodaNotificacao(BuildContext context,Map<String, dynamic> Dados){
