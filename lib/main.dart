11362bdf45298c7786d16ba09f83cb977186243b
Marlos <marlostrinidad@gmail.com>
2021-07-09 10:11:25 -0300

    atualizado pt2

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,36 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'dart:async';
-import 'dart:convert';
-
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:motoboybug_app/permission.dart';
-import 'package:motoboybug_app/token_monitor.dart';
 
-import 'Model/Pedido.dart';
 import 'Notificacao/NotificacaoDialog.dart';
-import 'menssage.dart';
-import 'menssage_list.dart';
+import 'Notificacao/PushNotificacao.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  // If you're going to use other Firebase services in the background, such as Firestore,
-  // make sure you call `initializeApp` before using other Firebase services.
+  print("Handling a background message: ${message.messageId}");
   await Firebase.initializeApp();
-  print('Handling a background message ${message.messageId}');
 }
 
-/// Create a [AndroidNotificationChannel] for heads up notifications
-const AndroidNotificationChannel channel = AndroidNotificationChannel(
-  'high_importance_channel', // id
-  'High Importance Notifications', // title
-  'This channel is used for important notifications.', // description
-  importance: Importance.high,
-);
 
-/// Initialize the [FlutterLocalNotificationsPlugin] package.
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-FlutterLocalNotificationsPlugin();
+AndroidNotificationChannel channel;
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
-Future<void> main() async {
+void  main()async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  // Set the background messaging handler early on, as a named top-level function
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
-  /// Create an Android Notification Channel.
-  ///
-  /// We use this channel in the `AndroidManifest.xml` file to override the
-  /// default FCM channel to enable heads up notifications.
+  channel = const AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    'This channel is used for important notifications.', // description
+    importance: Importance.high,
+  );
+
+  flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
   await flutterLocalNotificationsPlugin
       .resolvePlatformSpecificImplementation<
       AndroidFlutterLocalNotificationsPlugin>()
@@ -53,236 +38,83 @@ Future<void> main() async {
 
   /// Update the iOS foreground notification presentation options to allow
   /// heads up notifications.
-  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+  await FirebaseMessaging.instance
+      .setForegroundNotificationPresentationOptions(
     alert: true,
     badge: true,
     sound: true,
   );
 
-  runApp(MessagingExampleApp());
+  runApp(MyApp());
+
+
 }
 
-/// Entry point for the example application.
-class MessagingExampleApp extends StatelessWidget {
+
+class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Messaging Example App',
-      theme: ThemeData.dark(),
-      routes: {
-        '/': (context) => Application(),
-        '/message': (context) => MessageView(),
-      },
+      title: '',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+
+      ),
+      home: MyHomePage(title: 'Motoboy'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-// Crude counter to make messages unique
-int _messageCount = 0;
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key,  this.title}) : super(key: key);
 
-/// The API endpoint here accepts a raw FCM payload for demonstration purposes.
-String constructFCMPayload(String token) {
-  _messageCount++;
-  return jsonEncode({
-    'token': token,
-    'data': {
-      'via': 'FlutterFire Cloud Messaging!!!',
-      'count': _messageCount.toString(),
-    },
-    'notification': {
-      'title': 'Hello FlutterFire!',
-      'body': 'This notification (#$_messageCount) was created via FCM!',
-    },
-  });
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-/// Renders the example application.
-class Application extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _Application();
-}
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  String token;
+  FirebaseMessaging firebaseMessaging=FirebaseMessaging.instance;
+  PushNotificacao pushNotificacao= PushNotificacao();
 
-class _Application extends State<Application> {
-  String _token;
-  Pedido detalheCorrida=Pedido();
-  @override
-  void initState() {
-    super.initState();
-    FirebaseMessaging.instance
-        .getInitialMessage()
-        .then((RemoteMessage message) {
-      if (message != null) {
-        Navigator.pushNamed(context, '/message',
-            arguments: MessageArguments(message, true));
-      }
-    });
 
-    //notificação quando o app ta em primeiro plano
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      RemoteNotification notification = message.notification;
-      AndroidNotification android = message.notification?.android;
-      if (notification != null && android != null) {
-        detalheCorrida.lat_ponto="latponto.toString()";
-        detalheCorrida.long_ponto="longponto.toString()";
-        detalheCorrida.end_ponto="end_ponto";
-        detalheCorrida.nome_ponto="nome_ponto";
-        detalheCorrida.quant_itens="4";
-        detalheCorrida.telefone="telefone";
-        detalheCorrida.id_doc="id_Doc";
-        showDialog(
-            context:context,
-            barrierDismissible: false,
-            builder: (BuildContext context)=>NotificacaoDialog(detalheCorrida:detalheCorrida) );
-      }
-    });
-    //notificação quando o app ta em segundo plano
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      detalheCorrida.lat_ponto="latponto.toString()";
-      detalheCorrida.long_ponto="longponto.toString()";
-      detalheCorrida.end_ponto="end_ponto";
-      detalheCorrida.nome_ponto="nome_ponto";
-      detalheCorrida.quant_itens="4";
-      detalheCorrida.telefone="telefone";
-      detalheCorrida.id_doc="id_Doc";
-      showDialog(
-          context:context,
-          barrierDismissible: false,
-          builder: (BuildContext context)=>NotificacaoDialog(detalheCorrida:detalheCorrida) );
-    }
-    );
-  }
-
-  Future<void> sendPushMessage() async {
-    await FirebaseMessaging.instance.subscribeToTopic('teste_app');
-    if (_token == null) {
-      print('Unable to send FCM message, no token exists.');
-      return;
-    }
-
-    try {
-      await http.post(
-        Uri.parse('https://api.rnfirebase.io/messaging/send'),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-        body: constructFCMPayload(_token),
-      );
-      print('FCM request for device sent!');
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  Future<void> onActionSelected(String value) async {
-    switch (value) {
-      case 'subscribe':
-        {
-          print(
-              'FlutterFire Messaging Example: Subscribing to topic "fcm_test".');
-          await FirebaseMessaging.instance.subscribeToTopic('fcm_test');
-          print(
-              'FlutterFire Messaging Example: Subscribing to topic "fcm_test" successful.');
-        }
-        break;
-      case 'unsubscribe':
-        {
-          print(
-              'FlutterFire Messaging Example: Unsubscribing from topic "fcm_test".');
-          await FirebaseMessaging.instance.unsubscribeFromTopic('fcm_test');
-          print(
-              'FlutterFire Messaging Example: Unsubscribing from topic "fcm_test" successful.');
-        }
-        break;
-      case 'get_apns_token':
-        {
-          if (defaultTargetPlatform == TargetPlatform.iOS ||
-              defaultTargetPlatform == TargetPlatform.macOS) {
-            print('FlutterFire Messaging Example: Getting APNs token...');
-            String token = await FirebaseMessaging.instance.getAPNSToken();
-            print('FlutterFire Messaging Example: Got APNs token: $token');
-          } else {
-            print(
-                'FlutterFire Messaging Example: Getting an APNs token is only supported on iOS and macOS platforms.');
-          }
-        }
-        break;
-      default:
-        break;
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
+    pushNotificacao.initialize(context);
+    getToken();
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Cloud Messaging'),
-        actions: <Widget>[
-          PopupMenuButton(
-            onSelected: onActionSelected,
-            itemBuilder: (BuildContext context) {
-              return [
-                const PopupMenuItem(
-                  value: 'subscribe',
-                  child: Text('Subscribe to topic'),
-                ),
-                const PopupMenuItem(
-                  value: 'unsubscribe',
-                  child: Text('Unsubscribe to topic'),
-                ),
-                const PopupMenuItem(
-                  value: 'get_apns_token',
-                  child: Text('Get APNs token (Apple only)'),
-                ),
-              ];
-            },
-          ),
-        ],
+        title: Text(widget.title),
       ),
-      floatingActionButton: Builder(
-        builder: (context) => FloatingActionButton(
-          onPressed: sendPushMessage,
-          backgroundColor: Colors.white,
-          child: const Icon(Icons.send),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextButton(
+                onPressed:(){
+                  onActionSelected();
+                },
+                child: Text("Assinar Topico motoboyteste"))
+
+          ],
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(children: [
-          MetaCard('Permissions', Permissions()),
-          MetaCard('FCM Token', TokenMonitor((token) {
-            _token = token;
-            return token == null
-                ? const CircularProgressIndicator()
-                : Text(token, style: const TextStyle(fontSize: 12));
-          })),
-          MetaCard('Message Stream', MessageList()),
-        ]),
-      ),
+
     );
   }
-}
-
-/// UI Widget for displaying metadata.
-class MetaCard extends StatelessWidget {
-  final String _title;
-  final Widget _children;
-
-  // ignore: public_member_api_docs
-  MetaCard(this._title, this._children);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        width: double.infinity,
-        margin: const EdgeInsets.only(left: 8, right: 8, top: 8),
-        child: Card(
-            child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: Column(children: [
-                  Container(
-                      margin: const EdgeInsets.only(bottom: 16),
-                      child:
-                      Text(_title, style: const TextStyle(fontSize: 18))),
-                  _children,
-                ]))));
+  Future getToken()async{
+    token = await firebaseMessaging.getToken();
+    print("Token user: "+token);
+  }
+  Future<void> onActionSelected() async {
+    await FirebaseMessaging.instance.subscribeToTopic('motoboyteste');
+    print("assinou o topico");
   }
 }
